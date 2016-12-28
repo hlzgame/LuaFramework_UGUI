054e82a680f775006dc911c1e12f565af6e533f4
hlz <hlz@hlzdeMacBook-Pro.local>
2016-12-28 20:23:51 +0800

    添加一些数值逻辑

--- Assets/Resources/Lua/Manager/OreMapMgr.lua
+++ Assets/Resources/Lua/Manager/OreMapMgr.lua
@@ -1,7 +1,7 @@
 --
 -- Author: HLZ
 -- Date: 2016-11-22 16:49:08
--- 战斗控制器
+-- 矿场控制器
 
 require("Map.OreMapView")
 
@@ -20,6 +20,22 @@ end
 
 --初始化新的一轮数据
 function OreMapMgr:initData(  )
+    
+    --玩家当前所在的矿层
+    self.playerNowRow = 0
+    
+    --玩家最大到达的矿层
+    self.playerMaxRow = 0
+
+    --当前生成的最大矿层
+    self.maxRow = 0
+
+    --资源安全存在层数 最大存在N层的矿石
+    self.safeRow = 40
+
+
+
+
     -- body
     self:startGame()
 end
@@ -54,6 +70,40 @@ end
 function OreMapMgr:initCamera( ... )
 	
 end
+
+-----------------------------------------数据层的管理----------------------------------------
+
+function OreMapMgr:getPlayerNowRow( )
+    return self.playerNowRow
+end
+
+function OreMapMgr:setPlayerNowRow( playerNowRow )
+    self.playerNowRow = playerNowRow
+end
+
+
+function OreMapMgr:getPlayerMaxRow( )
+    return self.playerMaxRow
+end
+
+function OreMapMgr:setPlayerMaxRow( playerMaxRow )
+    self.playerMaxRow = playerMaxRow
+end
+
+function OreMapMgr:getMaxRow( )
+    return self.maxRow
+end
+
+function OreMapMgr:setMaxRow( maxRow )
+    self.maxRow = maxRow
+end
+
+function OreMapMgr:getSafeRow( )
+    return self.safeRow
+end
+
+
+
 
 
 return OreMapMgr
